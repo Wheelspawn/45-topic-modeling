ccf79076c39e0dec6b428aa454bb1994febbc55d
unknown <nataplane@gmail.com>
2018-10-06 13:28:25 -0500

    fixed encoding bug in load_corpus. fixed preprocessor.py bug.

--- tm_preprocessor/load_corpus.py
+++ tm_preprocessor/load_corpus.py
@@ -5,11 +5,11 @@ Created on Sat Oct  6 12:03:21 2018
 @author: Wheelspawn
 """
 
-
 import pandas as pd
 
 my_dir = 'data/'
 
-mit=pd.read_csv(my_dir+'MIT-IEEE-eBooks.csv')
-springer=pd.read_csv(my_dir+'Springer-eBooks.csv')
-wiley=pd.read_csv(my_dir+'Wiley-IEEE-eBooks.csv')
+mit=pd.read_csv(my_dir+'MIT-IEEE-eBooks.csv',encoding = "ISO-8859-1", header=0, sep=",")
+springer=pd.read_csv(my_dir+'Springer-eBooks.csv',encoding = "ISO-8859-1", header=0, sep=",")
+wiley=pd.read_csv(my_dir+'Wiley-IEEE-eBooks.csv',encoding = "ISO-8859-1", header=0, sep=",")
+
