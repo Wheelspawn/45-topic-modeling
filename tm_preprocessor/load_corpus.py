f0238e638988c266453fa32be68acb6b2131e3b5
unknown <nataplane@gmail.com>
2018-10-06 12:32:10 -0500

    loading .csv files

--- tm_preprocessor/load_corpus.py
+++ tm_preprocessor/load_corpus.py
@@ -8,5 +8,8 @@ Created on Sat Oct  6 12:03:21 2018
 
 import pandas as ps
 
-my_dir = '/data/'
+my_dir = 'data/'
 
+mit=pd.read_csv(my_dir+'MIT-IEEE-eBooks.csv')
+springer=pd.read_csv(my_dir+'Springer-eBooks.csv')
+wiley=pd.read_csv(my_dir+'Wiley-IEEE-eBooks.csv')
